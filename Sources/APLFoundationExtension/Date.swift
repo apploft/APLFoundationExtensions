a0d25f47b64ed6c5b4c203651b06e6f12695face
Philip Krück <philip.krueck@apploft.de>
2020-07-27 12:25:51 +0200

    TLN-2135 added Date extension

--- Sources/APLFoundationExtension/Date.swift
+++ Sources/APLFoundationExtension/Date.swift
@@ -77,3 +77,49 @@ public extension Date {
         }                
     }
 }
+
+extension Date {
+    static let OneHour: TimeInterval = 3600
+
+    /// Return the range of todays day
+    public static var todayRange: ClosedRange<Date> {
+        let calendar = Calendar.init(identifier: Locale.current.calendar.identifier)
+        let startOfDay = calendar.startOfDay(for: Date(timeIntervalSinceNow: 0))
+        let endOfDay = startOfDay.addingTimeInterval(OneHour * 24 - 1)
+
+        return startOfDay...endOfDay
+    }
+
+    /// Return the range of the current hour
+    public static var thisHourRange: ClosedRange<Date> {
+        let now = Date(timeIntervalSinceNow: 0)
+        let calendar = Calendar.current
+        var startOfHour = calendar.date(bySetting: .minute, value: 0, of: now) ?? now
+
+        if startOfHour != now {
+            // Calendar.date(bySetting: ..., value: ..., of: ...)
+            // rounds to the next hour when we set minute to 0
+            // we have to correct this by going back one hour again.
+            startOfHour = startOfHour.addingTimeInterval(-OneHour)
+        }
+
+        let endOfHour = startOfHour.addingTimeInterval(OneHour - 1)
+
+        return startOfHour...endOfHour
+    }
+
+    /// Return tomorrows date at the earliest possible time
+    public static var tomorrow: Date {
+        return todayRange.upperBound + 1
+    }
+
+    /// Return the date of the next hour based on the current hour
+    public static var nextHour: Date {
+        return thisHourRange.upperBound + 1
+    }
+    
+    /// Boolean value indicating whether the date is today.
+    public var isToday: Bool {
+        Self.todayRange.contains(self)
+    }
+}
